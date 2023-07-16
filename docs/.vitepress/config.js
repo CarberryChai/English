4f378409dc5882f5cecf9d3cb574cfc88ce45a29
Charlynn <CarberryChai@gmail.com>
2023-07-16 09:19:31 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+export default defineConfig({
+  title: 'English',
+  description: 'Notes',
+  themeConfig: {
+    siteTitle: false,
+    nav: [
+      { text: 'Grammar', link: '/' },
+      { text: 'Vocabulary', link: '/vocabulary/' },
+    ],
+    sidebar: {
+      '/vocabulary/': [
+        {
+          text: 'vocabulary 1...100',
+          link: '/vocabulary/chapter1',
+        },
+      ],
+      '/': [
+        {
+          text: 'Grammar',
+        },
+      ],
+    },
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/CarberryChai/English' },
+    ],
+  },
+})
